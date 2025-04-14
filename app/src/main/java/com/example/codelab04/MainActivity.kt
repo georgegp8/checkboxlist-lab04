f339ef2134d7dc487250c413777983ffa7d4b5d5
George Guerra <george.guerra@tecsup.edu.pe>
2025-04-13 22:31:24 -0500

    Trabajo con listas - ViewModel Finalizado

---
+++ app/src/main/java/com/example/codelab04/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.codelab04
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.example.codelab04.ui.theme.Codelab04Theme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Codelab04Theme {
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    content = { innerPadding ->
+                        WellnessScreen(modifier = Modifier.padding(innerPadding))
+                    }
+                )
+            }
+
+        }
+    }
+}
+
